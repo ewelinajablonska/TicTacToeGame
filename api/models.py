6e518799d1bef06263d9503426949c6ddadbcb9c
ewelinajablonska <ewelina.jablonska.pl@gmail.com>
2022-07-03 11:19:42 +0200

    create custom user profile

--- api/models.py
+++ api/models.py
@@ -1,3 +1,25 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext as _
+from django.conf import settings
 
-# Create your models here.
+class User(AbstractUser):
+    username = models.CharField(blank=True, null=True, max_length=60)
+    email = models.EmailField(_('email address'), unique=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    def __str__(self):
+        return "{}".format(self.email)
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='profile'
+    )
+    title = models.CharField(max_length=5)
+    address = models.CharField(max_length=255)
+    country = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    phone = models.CharField(max_length=40, blank=True)
--- config/settings.py
+++ config/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+AUTH_USER_MODEL = 'api.User'
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases

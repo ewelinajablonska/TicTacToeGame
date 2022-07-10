49c5e4d9e5db2b13f1c3eb09dda1adc3874a5152
ewelinajablonska <ewelina.jablonska.pl@gmail.com>
2022-07-10 19:21:48 +0200

    cleanup

--- api/admin.py
+++ api/admin.py
@@ -9,35 +9,8 @@ class UserProfileInline(admin.StackedInline):
     model = UserProfile
     can_delete = False
 
-
 @admin.register(User)
 class UserAdmin(BaseUserAdmin):
-    fieldsets = (
-        (None, {"fields": ("email", "password")}),
-        (_("Personal info"), {"fields": ("first_name", "last_name")}),
-        (
-            _("Permissions"),
-            {
-                "fields": (
-                    "is_active",
-                    "is_staff",
-                    "is_superuser",
-                    "groups",
-                    "user_permissions",
-                )
-            },
-        ),
-        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
-    )
-    add_fieldsets = (
-        (
-            None,
-            {
-                "classes": ("wide",),
-                "fields": ("email", "password1", "password2"),
-            },
-        ),
-    )
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)

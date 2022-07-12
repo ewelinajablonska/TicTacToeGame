ebfacc97fba79aa486968c70da76d6e26eedd1e0
ewelinajablonska <ewelina.jablonska.pl@gmail.com>
2022-07-12 23:38:29 +0200

    create auth tests

---
+++ conftest.py
@@ -0,0 +1,10 @@
+import pytest
+from model_bakery import baker
+
+from api.models import User, UserProfile
+
+@pytest.fixture
+def create_user(db, django_user_model):
+    def make_user(**kwargs):
+        return django_user_model.objects.create_user(**kwargs)
+    return make_user

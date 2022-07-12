c3cff7707c7603b09af0f53f71c0d832c077036e
ewelinajablonska <ewelina.jablonska.pl@gmail.com>
2022-07-12 23:36:07 +0200

    customize django-admin users menagment

--- api/admin.py
+++ api/admin.py
@@ -14,8 +14,33 @@ class UserAdmin(BaseUserAdmin):
     list_display = ("email", "first_name", "last_name", "is_staff")
     search_fields = ("email", "first_name", "last_name")
     ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email", "password1", "password2"),
+            },
+        ),
+    )
     inlines = (UserProfileInline,)
-
 
 admin.site.register(UserProfile)
 admin.site.register(HighScore)
